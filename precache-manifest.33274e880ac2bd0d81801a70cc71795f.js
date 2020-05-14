110e314d2358ba84a871790a5b61667940ca1afe
jessicaBuenfil <jessibuenfilsolis@gmail.com>
2020-05-14 11:07:26 -0500

    Updates

---
+++ precache-manifest.33274e880ac2bd0d81801a70cc71795f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0115609e32182b28777d182dfc1a302f",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "c646424ee25f5432b3ce",
+    "url": "/portfolio-v2/static/css/2.3c44bdb2.chunk.css"
+  },
+  {
+    "revision": "68d3169ae576afded035",
+    "url": "/portfolio-v2/static/css/main.eadfeaa9.chunk.css"
+  },
+  {
+    "revision": "c646424ee25f5432b3ce",
+    "url": "/portfolio-v2/static/js/2.c21a4775.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/portfolio-v2/static/js/2.c21a4775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d3169ae576afded035",
+    "url": "/portfolio-v2/static/js/main.95ed9763.chunk.js"
+  },
+  {
+    "revision": "bdff9f83c3df65942a99",
+    "url": "/portfolio-v2/static/js/runtime-main.bf86b5da.js"
+  },
+  {
+    "revision": "e493e540baef6c0709f6babdc705215f",
+    "url": "/portfolio-v2/static/media/coati.e493e540.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-v2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-v2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-v2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-v2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-v2/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2189297ce448ab7456af2445216e724f",
+    "url": "/portfolio-v2/static/media/profile.2189297c.png"
+  },
+  {
+    "revision": "3c14cdfe1700274045c7b99cc518bf6c",
+    "url": "/portfolio-v2/static/media/programming.3c14cdfe.png"
+  }
+]);
